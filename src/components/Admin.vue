ec0b9e11be9599ba334975969bd1bdff6b083dbf
Jonathan <jonathan@Neos-MacBook-Pro.local>
2019-11-13 13:02:05 -0500

    added

---
+++ src/components/Admin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="hello">
+        <h1>Welcome Administrator</h1>
+        <h2>{{msg}}</h2>
+    </div>    
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg: 'what up lmao'
+        }
+    }
+}
+</script>
---
+++ src/components/UserBoard.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="hello">
+    <h1>Welcome</h1>
+    <h2>{{msg}}</h2>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msg: 'filthy commoners'
+        }
+    }
+}
+</script>
