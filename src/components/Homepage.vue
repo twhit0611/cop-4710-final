0fbb2fe59b29b38f11b389d0defd744ed447274e
Andrew Glisson <andrew.glisson1029@gmail.com>
2019-11-19 17:01:30 -0500

    Update Homepage.vue

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -13,11 +13,33 @@
           <v-btn text  v-on:click="handleLogout">Logout</v-btn>
           </v-app-bar>
     <v-container fluid> 
-      <p>Welcome to CampusEvents</p>
-      <v-btn color="secondary" :to="{name: 'dashboard'}">To Dashboard</v-btn>
-      <v-btn color="secondary" :to="{name: 'admin'}">To Admin Dashboard</v-btn>
-      <v-btn color="secondary" :to="{name: 'superadmin'}">To Super-Admin Dashboard</v-btn>
-    </v-container>
+      <v-container>
+        <p class="display-4 font-weight-thin">Welcome to CampusEvents</p>
+        <v-icon large>school</v-icon>
+      </v-container>
+      <v-divider></v-divider>
+      <v-container>
+        <v-btn color="secondary" :to="{name: 'dashboard'}">To Dashboard</v-btn>
+        <v-btn color="secondary" :to="{name: 'admin'}">To Admin Dashboard</v-btn>
+        <v-btn color="secondary" :to="{name: 'superadmin'}">To Super-Admin Dashboard</v-btn>
+      </v-container>
+      <v-row align="center" justify="space-around">
+          <v-img
+            src="https://images.unsplash.com/photo-1498243691581-b145c3f54a5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+            aspect-ratio="1"
+            class="grey lighten-2"
+            max-width="400"
+            max-height="250"
+          ></v-img>  
+          <v-img
+            src="https://images.unsplash.com/photo-1485182708500-e8f1f318ba72?ixlib=rb-1.2.1&auto=format&fit=crop&w=1085&q=80"
+            aspect-ratio="1"
+            class="grey lighten-2"
+            max-width="400"
+            max-height="250"
+          ></v-img>  
+      </v-row> 
+     </v-container>
   </v-app>
 
   <!-- <div class="main">
