ac064dda0d74103e14891401ae2836ab45c747f9
m4gicpur22 <Affner@Knights.ucf.edu>
2019-11-18 21:03:31 -0500

    Schemas for events and comments

--- src/server/db.js
+++ src/server/db.js
@@ -1,20 +1,52 @@
 "use strict";
 const sqlite3 = require('sqlite3').verbose();
-const Users = require('./Schemas/User'); //the data we have in this file will go in here eventually
-const Events = require('./Schemas/Events');
-
 
 //this is our general database for now but in the future, we will need to seperate
 //and create individual tables, thus creating indivi. schemas such as above
 
 class DB {
+
     constructor(file) {
         this.DB = new sqlite3.Database(file);
         this.createTable();
-        
     }
 
-    createTable() {
+    //asynchronous methods
+    runAysnc(sql) {
+
+        var that = this;
+    
+        return new Promise( (resolve, reject) => {
+            that.get(sql, (err, row) => {
+
+                if (err)
+                    reject(err);
+                else
+                    resolve(row);
+            });
+        });
+    
+    };
+
+    getAysnc(sql) {
+
+        var that = this;
+    
+        return new Promise( (resolve, reject) => {
+            that.get(sql, (err, row) => {
+                if (err)
+                    reject(err);
+                else
+                    resolve(row);
+            });
+        });
+    
+    };
+
+
+    async createTable() {
+        try {
+
         const sql = `
             CREATE TABLE IF NOT EXISTS user (
                 id integer PRIMARY KEY, 
@@ -22,7 +54,15 @@ class DB {
                 email text UNIQUE, 
                 user_pass text,
                 admin_type integer)`
-        return this.DB.run(sql);
+                
+        return await this.DB.runAysnc(sql);
+
+        }
+        catch(err) {
+            console.log("Could not start database with error: "+ JSON.stringify(err));
+
+        }
+
     }
 
     selectByEmail(email, callback) {
@@ -54,7 +94,121 @@ class DB {
             user, (err) => {
                 callback(err)
             })
+    }
+
+    async createEvents() 
+    {   
+    
+    try {
+        var createEventstable = (
+                `CREATE TABLE IF NOT EXISTS Events(
+                Eventid integer PRIMARY KEY, 
+                name text,
+                decription text,
+                type text,
+                category text,
+                time, integer,
+                date integer,
+                is_Admin integer,
+                count_email integer,
+                count_phone integer
+                )`
+    );
+
+    return await this.EventsDB.runAysnc(createEventstable);
+    }
+    catch(err){
+        return console.error(err);
+    }
+
+}
+
+    insertEvents(Event, callback){
+        return this.DB.run(`INSERT INTO Events (name,description,type,category,is_Admin) VALUES (?,?,?,?,?)`, [Event], (err) => {
+            callback(err);
+        }); 
+    }
+
+    selectByEventName(name, callback){
+        return this.EventsDB.get(`SELECT * FROM Events WHERE name = ?`, [name], (err, row) => {
+            callback(err, row);
+        });
+    };
+
+    async createCommentstable(){
+
+        try {
+            var createComments = (
+                `CREATE TABLE IF NOT EXISTS Comments(
+                    CommentsID integer primary key,
+                    comment text,
+                    time integer,
+                    date integer,
+                    is_Admin integer
+                )`
+            );
+
+            return await this.CommentsDB.runAysnc(createComments);
+
         }
+        catch(err) {
+
+            return console.error(err);
+        }
+    }
+
+    async createRSOevent(){
+        try {
+    
+            var createRSO = (`
+            CREATE TABLE IF NOT EXISTS RSO(
+            RSOid integer,
+            name text,
+            description text,
+            type text,
+            category text,
+            time, integer,
+            date integer,
+            is_Admin integer,
+            count_email integer,
+            count_phone integer
+            )
+            `);
+    
+            return await this.EventsDB.runAysnc(createRSO);
+    
+        }
+    
+        catch(err){
+            console.log("There was a problem creating an RSO event in the database");
+        }
+        
+    }
+    
+    insertintoRSO(RSO, callback){
+            var insertRSOqueue = (
+                ` 'INSERT INTO Events (name,description,type,category,is_Admin) VALUES (?,?,?,?,?)' `
+            );
+                
+            return this.EventsDB.run(insertRSOqueue, [RSO], (err) => {
+                callback(err);
+            });
+    }
+    
+    selectByRSOName(name, callback){
+    
+        var selectRSOquery = (
+            ` SELECT * FROM RSO WHERE name = ?`
+        );
+    
+        return this.EventsDB.get(selectRSOquery, [name], (err, row) => {
+    
+            callback(err, row);
+    
+        });
+    };
+
+
 }
 
 module.exports = DB;
